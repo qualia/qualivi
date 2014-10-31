521542d6a02c5e79eaa1506cfbc56abb28042e24
Drew Carey Buglione <drew@drewb.ug>
2014-10-31 15:12:54 -0700

    `android create project --gradle --gradle-version 0.12.2 --target android-10 --name QualiVi --path qualivi --activity QualiVi --package ug.drewb.qualivi`

---
+++ src/main/java/ug/drewb/qualivi/QualiVi.java
@@ -0,0 +1,15 @@
+package ug.drewb.qualivi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QualiVi extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
