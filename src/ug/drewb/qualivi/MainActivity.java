e8a5fe5adffb6da5fa726f2ea740d46f85b6bdd2
Drew Carey Buglione <drew@drewb.ug>
2015-11-12 18:03:13 -0500

    WIP

--- src/ug/drewb/qualivi/MainActivity.java
+++ src/ug/drewb/qualivi/MainActivity.java
@@ -2,13 +2,33 @@ package ug.drewb.qualivi;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Vibrator;
 
 public class MainActivity extends Activity {
+
+    Vibrator vibrator;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        long[] pattern = new long[] { 1, 2, 3, 2, 1 };
+        vibrator.vibrate(pattern, 0);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        vibrator.cancel();
     }
 
 }
